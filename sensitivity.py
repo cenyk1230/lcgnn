491e1bcaec0122b3577f56da500362961affffe8
Yukuo Cen <cenyk1230@qq.com>
2021-02-07 01:20:01 +0800

    Add ego size argument

--- sensitivity.py
+++ sensitivity.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     parser.add_argument('--cuda', type=str, default=0)
     parser.add_argument('--model', type=str, default='transformer')
     parser.add_argument('--dataset', type=str, default='flickr')
+    parser.add_argument('--ego_size', default=[64], type=int, nargs='+')
     args = parser.parse_args()
     print(args)
 
@@ -35,7 +36,7 @@ if __name__ == "__main__":
         config["early_stopping"] = 20
 
     for seed in range(num):
-        for ego_size in [16, 32, 64, 128, 256]:
+        for ego_size in args.ego_size:
             cur_command = command + " ".join([f"--{key} {value}" if key != 'ego_size' else f"--{key} {ego_size}" for key, value in config.items()]) + f" --seed {seed}"
             print(cur_command)
 
